ac945e9397ebd0202560cab35d8190720ab7ca42
464david464 <davidmenachem464@gmail.com>
2022-05-10 21:22:10 +0300

    create-and-read

--- create.js
+++ create.js
@@ -2,9 +2,17 @@ const fs = require('fs');
 const cart = require('./read');
 
 function create(product) {
-    const newCart = [cart];
-    
-    newCart.push(product)
-}
 
-module.exports = cart;
+    const newCart = [];
+
+    if(cart() != '') {
+        newCart.push(cart())
+    }
+    newCart.push(product)
+
+    console.log(newCart);
+
+    fs.writeFileSync('./data/cart.json', (newCart))
+
+}
+    module.exports = create;
--- read.js
+++ read.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function read() {
     const cart = fs.readFileSync('./data/cart.json', 'utf-8');
-
+    
     return cart;
 }
 

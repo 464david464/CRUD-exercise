428771766f10e133d824b759589efca81e0b97a0
david weinstein <davidmenachem464@gmail.com>
2022-05-15 20:28:08 +0300

    .

--- create.js
+++ create.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const read = require('./read');
 
 function create(product) {
-<<<<<<< HEAD
-    cart() // if cart === '', creating '[]'
+    read() // if cart === '', creating '[]'
 
-    const newCart = JSON.parse(cart());
+    const newCart = JSON.parse(read());
     
     newCart.push(product)
     fs.writeFileSync('./data/cart.json', JSON.stringify(newCart))
@@ -14,20 +13,3 @@ console.log(newCart);
 }
 
 module.exports = create;
-=======
-
-    const cart = JSON.parse(read())
-
-    if(cart[0] == undefined) {
-        cart.push(product)
-    }
-
-    cart.push(cart[0], product)
-    
-    console.log(cart);
-
-    fs.writeFileSync('./data/cart.json', cart)
-
-}
-    module.exports = create;
->>>>>>> e34b23e9edd40d39163d05ea92d01c778b5b2ba0
--- read.js
+++ read.js
@@ -2,15 +2,11 @@ const fs = require('fs');
 
 function read() {
     const cart = fs.readFileSync('./data/cart.json', 'utf-8');
-<<<<<<< HEAD
 
     if(cart === '') {
         fs.writeFileSync('./data/cart.json', '[]')
     }
 
-=======
-    
->>>>>>> e34b23e9edd40d39163d05ea92d01c778b5b2ba0
     return cart;
 }
 
